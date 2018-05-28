b0ebb7c2e4a552a6cd54a9676b4739a0f0d64898
Alber Erre <arblugo@gmail.com>
2018-05-28 12:13:10 +0100

    Delete particles.js

--- src/components/particles.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const particlesJS = window.particlesJS;
-
-componentDidMount(){
-    particlesJS.load('particles', 'witnet-particles.json');
-}
-
-export default particles;
